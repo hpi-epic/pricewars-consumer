9d2e627fe185231fd632bb93dfdd535e88cbafb1
jaSunny <github@sunny.remote.mx>
2016-11-15 15:56:00 +0100

    adding CORS support..

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post "register", to: "register#create"
 
   post "setting", to: "setting#create"
+  put "setting", to: "setting#create"
   delete "setting", to: "setting#delete"
 
   get "behavior", to: "behavior#index"
