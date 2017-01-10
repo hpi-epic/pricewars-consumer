e4cfd6e6b92b982608fed08123def6b1d9c72ff5
jaSunny <github@alias.framsteg.org>
2017-01-10 17:47:08 +0100

    TDD und so ;D

--- spec/controllers/behavior_controller_spec.rb
+++ spec/controllers/behavior_controller_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe BehaviorController, type: :controller do
     it "response with JSON body containing expected behavior attributes" do
       hash_body = JSON.parse(response.body)
       # expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
-      expect(hash_body).to match_array([{"name" => "first", "description" => "I am buying the first possible item"},
-                                        {"name" => "random", "description" => "I am buying random items"},
-                                        {"name" => "cheap", "description" => "I am buying the cheapest item"},
-                                        {"name" => "expensive", "description" => "I am buying the most expensive item"},
-                                        {"name" => "cheap_and_prime", "description" => "I am buying the cheapest item which supports prime shipping"}])
+      expect(hash_body).to match_array([{"name"=>"first", "description"=>"I am buying the first possible item", "amount"=>20},
+                                        {"name"=>"random", "description"=>"I am buying random items", "amount"=>20},
+                                        {"name"=>"cheap", "description"=>"I am buying the cheapest item", "amount"=>20},
+                                        {"name"=>"expensive", "description"=>"I am buying the most expensive item", "amount"=>20},
+                                        {"name"=>"cheap_and_prime", "description"=>"I am buying the cheapest item which supports prime shipping", "amount"=>20}])
     end
   end
 end
