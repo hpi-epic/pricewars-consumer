60054ece346708ef08e4138cfa779f54d2b8096b
jaSunny <github@alias.framsteg.org>
2017-01-31 09:45:08 +0100

    fixing issue that consumer settings are not returned correctly rather the default settings are

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -12,39 +12,33 @@ class SettingController < BehaviorController
                                 pool_size:    300,
                                 idle_timeout: 10,
                                 keep_alive:   30
+
   def init(params, request)
-    @min_buying_amount              = params.key?(:min_buying_amount)              ? params[:min_buying_amount]              : 1
-    @max_buying_amount              = params.key?(:max_buying_amount)              ? params[:max_buying_amount]              : 1
-    @min_wait                       = params.key?(:min_wait)                       ? params[:min_wait]                       : 0.1
-    @max_wait                       = params.key?(:max_wait)                       ? params[:max_wait]                       : 2
-    @timeout_if_no_offers_available = params.key?(:timeout_if_no_offers_available) ? params[:timeout_if_no_offers_available] : 2
-    @consumer_per_minute            = params.key?(:consumer_per_minute)            ? params[:consumer_per_minute]            : 100.0
-    @max_req_per_sec                = params.key?(:max_req_per_sec)                ? params[:max_req_per_sec]                : 10
-    @timeout_if_too_many_requests   = params.key?(:timeout_if_too_many_requests)   ? params[:timeout_if_too_many_requests]   : 30
-    @amount_of_consumers            = params.key?(:amount_of_consumers)            ? params[:amount_of_consumers]            : 1
-    @probability_of_sell            = params.key?(:probability_of_sell)            ? params[:probability_of_sell]            : 100
-    @max_buying_price               = params.key?(:max_buying_price)               ? params[:max_buying_price]               : 80
-    @behaviors_settings             = params.key?(:behaviors)                      ? params[:behaviors]                      : gather_available_behaviors
+    $min_buying_amount              = params.key?(:min_buying_amount)              ? params[:min_buying_amount]              : 1
+    $max_buying_amount              = params.key?(:max_buying_amount)              ? params[:max_buying_amount]              : 1
+    $min_wait                       = params.key?(:min_wait)                       ? params[:min_wait]                       : 0.1
+    $max_wait                       = params.key?(:max_wait)                       ? params[:max_wait]                       : 2
+    $timeout_if_no_offers_available = params.key?(:timeout_if_no_offers_available) ? params[:timeout_if_no_offers_available] : 2
+    $consumer_per_minute            = params.key?(:consumer_per_minute)            ? params[:consumer_per_minute]            : 100.0
+    $max_req_per_sec                = params.key?(:max_req_per_sec)                ? params[:max_req_per_sec]                : 10
+    $timeout_if_too_many_requests   = params.key?(:timeout_if_too_many_requests)   ? params[:timeout_if_too_many_requests]   : 30
+    $amount_of_consumers            = params.key?(:amount_of_consumers)            ? params[:amount_of_consumers]            : 1
+    $probability_of_sell            = params.key?(:probability_of_sell)            ? params[:probability_of_sell]            : 100
+    $max_buying_price               = params.key?(:max_buying_price)               ? params[:max_buying_price]               : 80
+    $behaviors_settings             = params.key?(:behaviors)                      ? params[:behaviors]                      : gather_available_behaviors
     $marketplace_url                = params[:marketplace_url]
-    @consumer_url                   = request.original_url
+    $consumer_url                   = request.original_url
   end
 
   def index
-    settings = {}
-    settings["consumer_per_minute"]            = @consumer_per_minute            ? @consumer_per_minute            : 100.0
-    settings["max_req_per_sec"]                = @max_req_per_sec                ? @max_req_per_sec                : 10
-    settings["marketplace_url"]                = $marketplace_url                ? $marketplace_url                : "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace"
-    settings["amount_of_consumers"]            = @amount_of_consumers            ? @amount_of_consumers            : 1
-    settings["probability_of_sell"]            = @probability_of_sell            ? @probability_of_sell            : 100
-    settings["min_buying_amount"]              = @min_buying_amount              ? @min_buying_amount              : 1
-    settings["max_buying_amount"]              = @max_buying_amount              ? @max_buying_amount              : 1
-    settings["min_wait"]                       = @min_wait                       ? @min_wait                       : 0.1
-    settings["max_wait"]                       = @max_wait                       ? @max_wait                       : 2
-    settings["behaviors"]                      = @behaviors_settings             ? @behaviors_settings             : gather_available_behaviors
-    settings["timeout_if_no_offers_available"] = @timeout_if_no_offers_available ? @timeout_if_no_offers_available : 2
-    settings["timeout_if_too_many_requests"]   = @timeout_if_too_many_requests   ? @timeout_if_too_many_requests   : 30
-    settings["max_buying_price"]               = @max_buying_price               ? @max_buying_price               : 80
-    render json: settings
+    render json: retrieve_current_or_default_settings
+  end
+
+  def update
+    render(nothing: true, status: 405) && return unless request.content_type == "application/json"
+    render(nothing: true, status: 405) && return unless params.key?(:marketplace_url)
+    init(params, request)
+    index
   end
 
   def create
@@ -55,13 +49,13 @@ class SettingController < BehaviorController
     register_with_marketplace
 
     $list_of_threads ||= []
-    @amount_of_consumers.times do
+    $amount_of_consumers.times do
       thread = Thread.new do |_t|
         loop do
-          sleep((@consumer_per_minute / 60) + rand(@min_wait..@max_wait)) # sleep regarding global time zone and random offset
+          sleep(($consumer_per_minute / 60) + rand($min_wait..$max_wait)) # sleep regarding global time zone and random offset
           available_items = get_available_items
           if available_items == "[]"
-            sleep(@timeout_if_no_offers_available)
+            sleep($timeout_if_no_offers_available)
             next
           end
           status = logic(JSON.parse(available_items), params, params.key?("bulk") ? true : false)
@@ -70,7 +64,7 @@ class SettingController < BehaviorController
       $list_of_threads.push(thread)
     end
 
-    render(nothing: true, status: 200) && return
+    render json: retrieve_current_or_default_settings
   end
 
   def delete
@@ -80,7 +74,7 @@ class SettingController < BehaviorController
       end
       $list_of_threads = []
       deregister_with_marketplace
-      unless $marketplace_url.nil? or $consumer_id.nil?
+      unless $marketplace_url.nil? || $consumer_id.nil?
         render(nothing: true, status: 200) && return
       else
         render(text: "invalid configuration: consumer_id or marketplace_url unknown", status: 404) && return
@@ -96,13 +90,13 @@ class SettingController < BehaviorController
     url = $marketplace_url + "/consumers"
     puts url
     response = HTTParty.post(url,
-                             body:    {api_endpoint_url: @consumer_url,
+                             body:    {api_endpoint_url: $consumer_url,
                                        consumer_name:    "Consumer",
                                        description:      "Cool"
                                       }.to_json,
                              headers: {"Content-Type" => "application/json"})
     data = JSON.parse(response.body)
-    @consumer_token = data["consumer_token"]
+    $consumer_token = data["consumer_token"]
     $consumer_id    = data["consumer_id"]
   end
 
@@ -111,7 +105,7 @@ class SettingController < BehaviorController
     response = HTTParty.delete(url,
                                body:    {}.to_json,
                                headers: {"Content-Type"  => "application/json",
-                                         "Authorization" => "Token #{@consumer_token}"
+                                         "Authorization" => "Token #{$consumer_token}"
                                 })
   end
 
@@ -121,20 +115,20 @@ class SettingController < BehaviorController
     HTTParty.get(url).body
   end
 
-  def logic(items, settings, _bulk_boolean)
-    if rand(1..100) < @probability_of_sell
-      @behaviors_settings.each do |behavior| # decide on buying behavior based on settings
+  def logic(items, _settings, _bulk_boolean)
+    if rand(1..100) < $probability_of_sell
+      $behaviors_settings.each do |behavior| # decide on buying behavior based on settings
         if rand(1..100) < behavior[:amount]  # spread buying behavior accordingly to settings
-          item = BuyingBehavior.new(items, @max_buying_price).send("buy_" + behavior[:name]) # get item based on buying behavior
+          item = BuyingBehavior.new(items, $max_buying_price).send("buy_" + behavior[:name]) # get item based on buying behavior
           # Thread.new do |_subT|
           if item.nil?
-            sleep(@timeout_if_no_offers_available)
+            sleep($timeout_if_no_offers_available)
             break
           end
           status = execute(item, behavior[:name]) # buy now!
           puts status
           if status == 429
-            sleep(@timeout_if_too_many_requests)
+            sleep($timeout_if_too_many_requests)
           elsif status == 401
             register_with_marketplace
           end
@@ -152,14 +146,32 @@ class SettingController < BehaviorController
     puts url
     response = HTTParty.post(url,
                              body:    {price:       item["price"],
-                                       amount:      rand(@min_buying_amount..@max_buying_amount),
+                                       amount:      rand($min_buying_amount..$max_buying_amount),
                                        consumer_id: $consumer_id,
                                        prime:       item["prime"],
                                        behavior:    behavior
                                       }.to_json,
                              headers: {"Content-Type"  => "application/json",
-                                       "Authorization" => "Token #{@consumer_token}"
+                                       "Authorization" => "Token #{$consumer_token}"
                                       })
     response.code
+  end
+
+  def retrieve_current_or_default_settings
+    settings = {}
+    settings["consumer_per_minute"]            = $consumer_per_minute            ? $consumer_per_minute            : 100.0
+    settings["max_req_per_sec"]                = $max_req_per_sec                ? $max_req_per_sec                : 10
+    settings["marketplace_url"]                = $marketplace_url                ? $marketplace_url                : "http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace"
+    settings["amount_of_consumers"]            = $amount_of_consumers            ? $amount_of_consumers            : 1
+    settings["probability_of_sell"]            = $probability_of_sell            ? $probability_of_sell            : 100
+    settings["min_buying_amount"]              = $min_buying_amount              ? $min_buying_amount              : 1
+    settings["max_buying_amount"]              = $max_buying_amount              ? $max_buying_amount              : 1
+    settings["min_wait"]                       = $min_wait                       ? $min_wait                       : 0.1
+    settings["max_wait"]                       = $max_wait                       ? $max_wait                       : 2
+    settings["behaviors"]                      = $behaviors_settings             ? $behaviors_settings             : gather_available_behaviors
+    settings["timeout_if_no_offers_available"] = $timeout_if_no_offers_available ? $timeout_if_no_offers_available : 2
+    settings["timeout_if_too_many_requests"]   = $timeout_if_too_many_requests   ? $timeout_if_too_many_requests   : 30
+    settings["max_buying_price"]               = $max_buying_price               ? $max_buying_price               : 80
+    settings
   end
 end
--- spec/controllers/setting_controller_spec.rb
+++ spec/controllers/setting_controller_spec.rb
@@ -4,28 +4,50 @@ require "pp"
 RSpec.describe SettingController, type: :controller do
   describe "GET #settings", type: :request do
     before do
-      get_response =  '[{"offer_id":1,"product_id":"1","seller_id":"1","amount":3,"price":5,"shipping_time":5,"prime":true},'\
-                       '{"offer_id":1,"product_id":"1","seller_id":"1","amount":3,"price":5,"shipping_time":5,"prime":true}]'
+      get_response =  '[{"quality":1,"price":22.50,"amount":11,"offer_id":4407,"product_id":1,"uid":11,"merchant_id":"ddq6bUP+/Etxboc8OVC8B3QQxO8+Ca5e4kqYOChwNN4=","shipping_time":{"standard":5,"prime":1},"prime":true},'\
+                       '{"quality":2,"price":51.00,"amount":5,"offer_id":4467,"product_id":1,"uid":12,"merchant_id":"/CWsWojD9goF9R11o37j2C+2IXQojHRWAYEcXFyKiDo=","shipping_time":{"standard":1,"prime":1},"prime":true},'\
+                       '{"quality":1,"price":54.00,"amount":51,"offer_id":4457,"product_id":1,"uid":11,"merchant_id":"/CWsWojD9goF9R11o37j2C+2IXQojHRWAYEcXFyKiDo=","shipping_time":{"standard":1,"prime":1},"prime":true},'\
+                       '{"quality":2,"price":51.00,"amount":15,"offer_id":4461,"product_id":1,"uid":12,"merchant_id":"/CWsWojD9goF9R11o37j2C+2IXQojHRWAYEcXFyKiDo=","shipping_time":{"standard":1,"prime":1},"prime":true},'\
+                       '{"quality":3,"price":54.00,"amount":52,"offer_id":4465,"product_id":1,"uid":11,"merchant_id":"/CWsWojD9goF9R11o37j2C+2IXQojHRWAYEcXFyKiDo=","shipping_time":{"standard":1,"prime":1},"prime":true},'\
+                       '{"quality":4,"price":54.00,"amount":60,"offer_id":4491,"product_id":1,"uid":11,"merchant_id":"/CWsWojD9goF9R11o37j2C+2IXQojHRWAYEcXFyKiDo=","shipping_time":{"standard":1,"prime":1},"prime":true},'\
+                       '{"quality":3,"price":54.00,"amount":64,"offer_id":4468,"product_id":1,"uid":11,"merchant_id":"/CWsWojD9goF9R11o37j2C+2IXQojHRWAYEcXFyKiDo=","shipping_time":{"standard":1,"prime":1},"prime":true},'\
+                       '{"quality":1,"price":22.50,"amount":21,"offer_id":4548,"product_id":1,"uid":11,"merchant_id":"ddq6bUP+/Etxboc8OVC8B3QQxO8+Ca5e4kqYOChwNN4=","shipping_time":{"standard":5,"prime":1},"prime":true},'\
+                       '{"quality":4,"price":22.50,"amount":11,"offer_id":4569,"product_id":1,"uid":11,"merchant_id":"ddq6bUP+/Etxboc8OVC8B3QQxO8+Ca5e4kqYOChwNN4=","shipping_time":{"standard":5,"prime":1},"prime":true},'\
+                       '{"quality":1,"price":29.99,"amount":1,"offer_id":5696,"product_id":1,"uid":11,"merchant_id":"MpWsNBYFvUgqq+rI0FGDTPYJ/RLB9ED7KLmIQwGqzAk=","shipping_time":{"standard":5,"prime":1},"prime":true},'\
+                       '{"quality":2,"price":29.99,"amount":1,"offer_id":5695,"product_id":1,"uid":11,"merchant_id":"MpWsNBYFvUgqq+rI0FGDTPYJ/RLB9ED7KLmIQwGqzAk=","shipping_time":{"standard":5,"prime":1},"prime":true}]'
+
       stub_request(:get, /marketplace.api.mp_pricewars.com/).to_return(body: get_response, status: 200, headers: {})
       post_response = "{}"
       stub_request(:post, /marketplace.api.mp_pricewars.com/).to_return(body: post_response, status: 200, headers: {})
     end
 
     before do
-      params = {tick:                1,
+      @params = {tick:                1,
                 marketplace_url:     "http://marketplace.api.mp_pricewars.com",
-                amount_of_consumers: 2,
-                probability_of_sell: 50,
+                amount_of_consumers: 1,
+                probability_of_sell: 100,
                 behaviors:           [
                   {type:   "first",
-                   amount: 10}
+                   amount: 100}
                 ],
                 test:                true
       }
-      post "/setting/", params.to_json, "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"
     end
 
     it "returns http success" do
+      post "/setting/", @params.to_json, "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"
+      expect(response).to have_http_status(:success)
+    end
+
+    xit "behavior first" do
+      @params["behaviors"]["type"] = "first"
+      post "/setting/", @params.to_json, "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"
+      expect(response).to have_http_status(:success)
+    end
+
+    xit "behavior cheap" do
+      @params["behaviors"]["type"] = "cheap"
+      post "/setting/", @params.to_json, "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"
       expect(response).to have_http_status(:success)
     end
   end
