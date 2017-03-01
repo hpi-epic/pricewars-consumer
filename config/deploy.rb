9a32e4c68d3b55ce04d4cff5cfaed7c53cc9ea00
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-03-01 14:36:44 +0100

    Don't install gems specified in Gemfile on Deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,8 +51,8 @@ namespace :deploy do
   task :prepare_app do
     on roles :all do
       within release_path do
-        execute "gem install bundler"
-        execute "bundle install --gemfile=#{release_path}/Gemfile --deployment"
+        # execute "gem install bundler"
+        # execute "bundle install --gemfile=#{release_path}/Gemfile --deployment"
         # execute "cp #{release_path}/config/database.mysql.yml #{release_path}/config/database.yml"
         # execute :rake, "db:create"
         # execute :rake, "db:migrate"
