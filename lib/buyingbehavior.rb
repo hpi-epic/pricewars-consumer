57b3966ad27038e4db37a4978f3141fde5fbb7eb
Carsten Walther <carsten.walther@student.hpi.de>
2018-04-09 17:51:00 +0200

    use random function that supports floats; fixes prefer_cheap behavior

--- lib/buyingbehavior.rb
+++ lib/buyingbehavior.rb
@@ -201,7 +201,7 @@ class BuyingBehavior
     sum = weighted.inject(0) do |sum, item_and_weight|
       sum += item_and_weight[1]
     end
-    target = rand(sum)
+    target = Random.rand(sum)
     weighted.each do |item, weight|
       return item if target <= weight
       target -= weight
