5d8a7b3228fac6561ae2a5beb24a5d69653e8b28
Carsten Walther <carsten.walther@student.hpi.de>
2018-06-05 19:37:52 +0200

    make thread list a class variable instead of a global variable

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 Dotenv.load(File.expand_path('../.env',  __FILE__))
 
+# Show program output without delay (disable output buffering).
+STDOUT.sync = true
+
 producer = ENV['PRICEWARS_PRODUCER_URL'] || 'vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de'
 marketplace = ENV['PRICEWARS_MARKETPLACE_URL'] || 'vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace'
 
